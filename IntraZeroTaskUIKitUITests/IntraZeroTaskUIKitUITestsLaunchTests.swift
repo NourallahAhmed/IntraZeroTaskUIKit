66a69deee75a4a686826b7b0e238479f2c7175e4
NourallahAhmed <NourAllahahmed7001@gmail.com>
2022-08-14 18:05:08 -0700

    Initial Commit

---
+++ IntraZeroTaskUIKitUITests/IntraZeroTaskUIKitUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  IntraZeroTaskUIKitUITestsLaunchTests.swift
+//  IntraZeroTaskUIKitUITests
+//
+//  Created by NourAllah Ahmed on 14/08/2022.
+//
+
+import XCTest
+
+class IntraZeroTaskUIKitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
